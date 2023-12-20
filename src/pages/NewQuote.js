8b399713ef23d430948a6d476207264d1d140ffa
jayantsingh-22 <jayantsingh640@gmail.com>
2023-12-20 16:15:23 +0530

    added:logic for post and get req and res

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,16 +1,25 @@
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
 
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  const {sendRequest, status} = useHttp(addQuote);
   let navigate = useNavigate();
 
+  useEffect(() => {
+    if (status === "completed") {
+      navigate("/quotes");
+    }
+  }, [status, navigate]);
+
   const addQuoteHandler = (quoteData) => {
-    console.log(quoteData);
-    navigate("/quotes")
+    sendRequest(quoteData);
   };
 
-  return <QuoteForm onAddQuote={addQuoteHandler} />;
+  return <QuoteForm isLoading={status==='pending'} onAddQuote={addQuoteHandler} />;
 };
 
 export default NewQuote;
